d55e50c4d0c9c6a7ce7aafa1e0881c2d93501792
i857789 <natan.portilho@sap.com>
2020-05-19 13:21:11 -0300

    Creating start button and section where text should be displayed

--- typist-dream/src/app/app.component.ts
+++ typist-dream/src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'typist-dream';
+
+
+
+  start() {
+    const mainSection = document.getElementById('main-section');
+    mainSection.style.display = 'block';
+    const startButton = document.getElementById('start-button');
+    startButton.style.display= 'none';
+  }
+
 }
