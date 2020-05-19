75093dbee27ebea7ac589afbd269d366f4bda9a6
i857789 <natan.portilho@sap.com>
2020-05-19 13:40:11 -0300

    using hostListener to listen to keys pressed in keyboard

--- typist-dream/src/app/app.component.ts
+++ typist-dream/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,14 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'typist-dream';
-
-
+  text = 'natan portilho da silva';
+  typed = '';
 
   start() {
     const mainSection = document.getElementById('main-section');
     mainSection.style.display = 'block';
     const startButton = document.getElementById('start-button');
-    startButton.style.display= 'none';
+    startButton.style.display = 'none';
   }
 
+  @HostListener('document:keypress', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    const key = event.key;
+    this.typed = this.typed.concat(key);
+    console.log(this.typed);
+
+    if (this.text.length == this.typed.length) {
+
+      if (this.text == this.typed) {
+        console.log('sucess')
+      } else {
+        console.log('fail');
+      }
+    }
+  }
+
+  @HostListener('document:keydown', ['$event']) onKeydownHandler(event: KeyboardEvent) {
+    if (event.keyCode == 8) {
+      this.typed = this.typed.slice(0, -1);
+    }
+  }
 }
