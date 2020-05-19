8d3ca74b02820fe5e357a674ad2538f5388ff5a0
i857789 <natan.portilho@sap.com>
2020-05-19 15:29:28 -0300

    Adding functions to change color on text when user types correctly

--- typist-dream/src/app/app.component.ts
+++ typist-dream/src/app/app.component.ts
@@ -8,14 +8,18 @@ import { HostListener } from '@angular/core';
 })
 export class AppComponent {
   title = 'typist-dream';
-  text = 'natan portilho da silva';
+  text = '';
   typed = '';
+  originalText = '';
 
   start() {
     const mainSection = document.getElementById('main-section');
     mainSection.style.display = 'block';
     const startButton = document.getElementById('start-button');
     startButton.style.display = 'none';
+    const text = document.getElementById('text');
+    this.text = text.innerText
+    this.originalText = text.innerText;
   }
 
   @HostListener('document:keypress', ['$event'])
@@ -25,12 +29,18 @@ export class AppComponent {
     console.log(this.typed);
 
     if (this.text.length == this.typed.length) {
-
       if (this.text == this.typed) {
         console.log('sucess')
       } else {
         console.log('fail');
       }
+    }
+
+    const lastTypedIndex = this.typed.length - 1;
+    if (this.originalText[lastTypedIndex] == this.typed[lastTypedIndex]) {
+      this.fixColors(this.typed.length);
+    } else {
+      this.typed = this.typed.slice(0, -1);
     }
   }
 
@@ -38,5 +48,20 @@ export class AppComponent {
     if (event.keyCode == 8) {
       this.typed = this.typed.slice(0, -1);
     }
+  }
+
+  fixColors(position: Number) {
+    const textElement = document.getElementById("text");
+    const txt = this.originalText;
+    let newText = "";
+
+    for (let i = 0; i < this.originalText.length; i++) {
+      if (i < position) {
+        newText += '<span style="color:#2a9d8f' + '">' + txt.charAt(i) + '</span>';
+      } else {
+        newText += '<span style="color:#e63946' + '">' + txt.charAt(i) + '</span>';
+      }
+    }
+    textElement.innerHTML = newText;
   }
 }
